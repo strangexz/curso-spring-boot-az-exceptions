3835bc0fb152b4afc3a65e42ed7e181d552e8604
Strangexz <ja_figueroa@outlook.es>
2024-08-21 10:50:22 -0600

    feat: manejo de los errores not found

--- src/main/java/hn/com/jf/controllers/HandlerExceptionController.java
+++ src/main/java/hn/com/jf/controllers/HandlerExceptionController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import hn.com.jf.models.Error;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
@@ -16,8 +19,19 @@ public class HandlerExceptionController {
 		error.setError("Error division entre cero!");
 		error.setMessage(ex.getMessage());
 		error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-		
+
 //		return ResponseEntity.internalServerError().body(error);
+		return ResponseEntity.status(error.getStatus()).body(error);
+	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<Error> notFoundEx(NoHandlerFoundException ex) {
+		Error error = new Error();
+		error.setDate(new Date());
+		error.setError("Ruta no encontrada!");
+		error.setMessage(ex.getMessage());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		
 		return ResponseEntity.status(error.getStatus()).body(error);
 	}
 }
