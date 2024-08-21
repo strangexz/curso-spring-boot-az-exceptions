fdca68e7f463558e1afea1f9dfa9176ddbb6991a
Strangexz <ja_figueroa@outlook.es>
2024-08-21 11:27:08 -0600

    feat: otra forma de manejo de errores sin dto

--- src/main/java/hn/com/jf/controllers/AppController.java
+++ src/main/java/hn/com/jf/controllers/AppController.java
@@ -2,15 +2,22 @@ package hn.com.jf.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
+@RequestMapping("/app")
 public class AppController {
 	
-	@GetMapping("/app")
-	public String index() {
+	@GetMapping("/divisionByZero")
+	public String divisionByZero() {
 		int value = 100 / 0;
+		return "Ok";
+	}
+	
+	@GetMapping("/numberFormat")
+	public String numberFormat() {
+		int value = Integer.parseInt("10x");
 		return "Ok";
 	}
 	
--- src/main/java/hn/com/jf/controllers/HandlerExceptionController.java
+++ src/main/java/hn/com/jf/controllers/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package hn.com.jf.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -31,7 +34,19 @@ public class HandlerExceptionController {
 		error.setError("Ruta no encontrada!");
 		error.setMessage(ex.getMessage());
 		error.setStatus(HttpStatus.NOT_FOUND.value());
-		
+
 		return ResponseEntity.status(error.getStatus()).body(error);
+	}
+
+	@ExceptionHandler(NumberFormatException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> numberFormatException(Exception ex) {
+		Map<String, Object> error = new HashMap<>();
+		error.put("date", new Date());
+		error.put("error", "Formato de numero no valido.");
+		error.put("message", ex.getMessage());
+		error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		
+		return error;
 	}
 }
